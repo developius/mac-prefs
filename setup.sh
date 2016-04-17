9482a2896e0c16d16e6dcff49a6072cfe3e2512b
Finnian Anderson <finnian@fxapi.co.uk>
2016-04-17 18:42:05 +0100

    Add setup scripts and configs

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+copy_to_home=(".profile", ".bash_profile", ".gitconfig")
+
+for file in "${copy_to_home[@]}"
+do
+	cp "$file" ~/
+done
+
+. ~/.profile
+
+cp "Default (OSX).sublime-keymap" "/Users/finnian/Library/Application Support/Sublime Text 3/Packages/User"
+
+su admin
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install node
+
+npm install -g nodemon mongo
