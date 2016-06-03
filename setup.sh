86be927f398bf9792d4ed47923e58e0204b49c04
Finnian Anderson <finnian@fxapi.co.uk>
2016-06-03 19:32:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ done
 
 . ~/.profile
 
+
 cp "Default (OSX).sublime-keymap" "/Users/finnian/Library/Application Support/Sublime Text 3/Packages/User"
+cp "Preferences.sublime-settings" "/Users/finnian/Library/Application Support/Sublime Text 3/Packages/User"
 
 su admin
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
